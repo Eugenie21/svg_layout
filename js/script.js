fd74164517438411eb3fb5b7d07d17272bc4a8af
Eugenie21 <sarrahh789@gmail.com>
2015-03-29 00:05:14 +0400

    Second

--- js/script.js
+++ js/script.js
@@ -2,81 +2,68 @@ var SvgLayout = (function($) {
 
 	var modulesOnPage = {},
 		moduleMap = {
-			navBar: {
-				type: 'navBar',
-				modKey: '',
-				modContainer: '#',
-				modParams: {
-					links: [
-						{title: 'empty', link: 'empty'},
-						{title: 'empty', link: 'empty'},
-						{title: 'empty', link: 'empty'},
-						{title: 'empty', link: 'empty'},
-						{title: 'empty', link: 'empty'}
-					],
+			slBlock: function(module) {
+				this.type = module.type;
+				this.modkey = module.modkey;
+				this.containerName = module.containerName;
+				this.snapObj = null;
+				this.content = module.content;
+				this.viewParams = {
 					color: {
-						text: '#000',
-						back: '#ccc',
-						border: '#000'
+						text: '#fff',
+						back: 'none',
+						border: '#4B4B4B'
 					},
 					size: {
 						text: '15',
-						border: '2'
+						border: '6'
 					},
-					speed: '1000'
-				},
-				snapContainer: null,
-				init: null
-			},
-			sideBar: {
-				type: 'sideBar',
-				modKey: '',
-				modContainer: '#',
-				modParams: {
-					links: [
-						{title: 'empty', link: 'empty'},
-						{title: 'empty', link: 'empty'},
-						{title: 'empty', link: 'empty'},
-						{title: 'empty', link: 'empty'},
-						{title: 'empty', link: 'empty'}
-					],
-					color: {
-						text: '#000',
-						back: '#ccc',
-						border: '#000'
-					},
-					size: {
-						text: '15',
-						border: '2'
-					},
-					speed: '1000'
-				},
-				snapContainer: null,
-				init: null
+					animationSpeed: '1000'
+				};
+				this.draw = function(){ 
+
+					switch(module.type) {
+						case 'navbar': methodMap.drawNav(module.modkey)
+							break
+						case 'sidebar': methodMap.drawSide(module.modkey)
+							break
+						default: console.log("No such module '"+module.type+"'")
+							break
+					}
+				}
 			}
 		},
 		methodMap = {
 			svgInit: null,
-			moduleInit: null
+			moduleInit: null,
+
+			drawNav: null,
+			drawSide: null,
+
+			makeLink: null,
+			// stringSize: null
 		};
 
 
-	methodMap.moduleInit = function(module, modType) {
+	methodMap.moduleInit = function(module) {
 
-		modulesOnPage[module.modKey] = moduleMap[modType];
+		modulesOnPage[module.modkey] = new moduleMap.slBlock(module);
 
-		modulesOnPage[module.modKey].modKey = module.modKey;
-		modulesOnPage[module.modKey].modContainer = module.modContainer;
-
-		for(var key in module.modParams) {
-			if(modulesOnPage[module.modKey].modParams.hasOwnProperty(key)) {
-				modulesOnPage[module.modKey].modParams[key] = module.modParams[key];
+		for(var key in module.viewParams) {
+			if(modulesOnPage[module.modkey].viewParams.hasOwnProperty(key)) {
+				if(modulesOnPage[module.modkey].viewParams[key].length > 1){
+					for(var key2 in module.viewParams[key]) {
+						modulesOnPage[module.modkey].viewParams[key][key2] = module.viewParams[key][key2];
+					}
+				}else{
+					modulesOnPage[module.modkey].viewParams[key] = module.viewParams[key];
+				}
 			}
 		}
 
-		var params = modulesOnPage[module.modKey].modParams;
+		var params = modulesOnPage[module.modkey].viewParams;
 
-		jQuery(modulesOnPage[module.modKey].modContainer).css({
+		jQuery(modulesOnPage[module.modkey].containerName).css({
 			position: 'absolute',
 			top: '0',
 			left: '0',
@@ -84,19 +71,59 @@ var SvgLayout = (function($) {
 			height: '100%'
 		});
 
-		modulesOnPage[module.modKey].snapContainer = new Snap(modulesOnPage[module.modKey].modContainer);
-
-		modulesOnPage[module.modKey].init();
+		modulesOnPage[module.modkey].snapObj = new $(modulesOnPage[module.modkey].containerName);
+		modulesOnPage[module.modkey].draw();
 	}
 
-	//NAVBAR
-	moduleMap.navBar.init = function(p) {
+	// methodMap.stringSize = function(string) {
 
-		console.log(this);
+	// }
+
+	//NAVBAR
+	methodMap.drawNav = function(modkey) {
+
+		if(!(modulesOnPage[modkey].content.links.length >  1)) {
+			console.log('No Links in module '+ modkey);
+			return false;
+		}
+
+		var blockHeight = parseInt(jQuery(modulesOnPage[modkey].containerName).height()),
+			blockWidth = parseInt(jQuery(modulesOnPage[modkey].containerName).width()),
+			borderWidth = modulesOnPage[modkey].viewParams.size.border
+			links = modulesOnPage[modkey].content.links.length,
+			linkWidth = blockWidth/links - borderWidth,
+			linkHeight = blockHeight-borderWidth*2,
+			borderPath = [];
+
+			for(var i=0; i<links; i++) {
+
+				if(i==0) {
+					borderPath.push('M '+(borderWidth/2+linkWidth*i+borderWidth*i)+' '+(borderWidth/2)
+									+'L '+(borderWidth/2+linkWidth*i+borderWidth*i)+' '+(linkHeight+borderWidth/2)
+									+', '+(borderWidth/2+linkWidth*(i+1)+borderWidth*i)+' '+(linkHeight+borderWidth/2)
+									+', '+(borderWidth/2+linkWidth*(i+1)+borderWidth*i)+' '+(borderWidth/2)
+									+', '+(linkWidth*i+borderWidth*i)+' '+(borderWidth/2)); //Не убирать 'i' --- Потому-что !!!
+				}else{
+					borderPath.push('M '+(borderWidth*i+linkWidth*i)+' '+(linkHeight+borderWidth/2)
+									+'L '+(borderWidth*i+linkWidth*(i+1)+borderWidth/2)+' '+(linkHeight+borderWidth/2)
+									+', '+(borderWidth*i+linkWidth*(i+1)+borderWidth/2)+' '+(borderWidth/2)
+									+', '+(linkWidth*i+borderWidth*i)+' '+(borderWidth/2));
+				}
+			}
+
+			for(i in borderPath) {
+				modulesOnPage[modkey].snapObj.path(borderPath[i]).attr({
+					stroke: modulesOnPage[modkey].viewParams.color.border,
+					fill: modulesOnPage[modkey].viewParams.color.back,
+					strokeWidth: borderWidth
+				});
+			}
+
+		console.log(borderPath);
 	}
 
 	//SIDEBAR
-	moduleMap.sideBar.init = function(p) {
+	methodMap.drawSide = function(modkey) {
 
 	}
 
@@ -104,16 +131,11 @@ var SvgLayout = (function($) {
 
 		for (var key in modules) {
 
-			if(moduleMap.hasOwnProperty(key)) {
-				if(modules[key].length > 1) {
-					for(var key2 in modules[key]) {
-						methodMap.moduleInit(modules[key][key2],key);
-					}
-				}else {
-					methodMap.moduleInit(modules[key],key);
-				}
+			if(modules[key].type && modules[key].modkey && modules[key].containerName) {
+				methodMap.moduleInit(modules[key]);
 			}
 		}
+
 		console.log('Modules On Page: ');
 		for(key in modulesOnPage) {
 			console.log(modulesOnPage[key]);
@@ -129,52 +151,73 @@ var SvgLayout = (function($) {
 
 document.addEventListener("DOMContentLoaded", function () {
 
-	SvgLayout.init({
-
-		navBar: [
-			{
-				modKey: 'navBar1',
-				modContainer: '#slNavBar',
-				modParams: {
-					links: [
-						{title: 'Главная', link: '#'},
-						{title: 'Продукты', link: '#'},
-						{title: 'Доставка', link: '#'},
-						{title: 'О нас', link: '#'},
-						{title: 'Телефон', link: '#'}
-					]
-				}
-			},{
-				modKey: 'navBar2',
-				modContainer: '#slNavBar1',
-				modParams: {
-					links: [
-						{title: 'Главная', link: '#'},
-						{title: 'Продукты', link: '#'},
-						{title: 'Доставка', link: '#'},
-						{title: 'О нас', link: '#'},
-						{title: 'Телефон', link: '#'}
-					]
+	SvgLayout.init([
+		{
+			type: 'navbar',
+			modkey: 'navbar12',
+			containerName: '#slNavBar',
+			content: {
+				links: [
+					{title: 'Главная', link: '#'},
+					{title: 'Продукты', link: '#'},
+					{title: 'Доставка', link: '#'},
+					{title: 'О нас', link: '#'},
+					{title: 'Телефон', link: '#'}
+				]
+			},
+			// viewParams: {
+			// 	color: {
+			// 		text: '#000',
+			// 		back: '#ffa',
+			// 		border: '#000'
+			// 	}
+			// }
+		},{
+			type: 'navbar',
+			modkey: 'navbar1',
+			containerName: '#slNavBar1',
+			content: {
+				links: [
+					{title: 'Главная', link: '#'},
+					{title: 'Продукты', link: '#'},
+					{title: 'Доставка', link: '#'},
+					{title: 'О нас', link: '#'},
+					{title: 'Телефон', link: '#'},
+					{title: 'Телефон', link: '#'},
+					{title: 'Телефон', link: '#'}
+				]
+			},
+			viewParams: {
+				color: {
+					text: '#000',
+					back: '#ffa',
+					border: '#000'
 				}
 			}
-		],
-		sideBar: [
-			{
-				modKey: 'sideBar1',
-				modContainer: '#slSideBar',
-				modParams : {
-					links: [
-						{title: 'Главная', link: '#'},
-						{title: 'Продукты', link: '#'},
-						{title: 'Доставка', link: '#'},
-						{title: 'О нас', link: '#'},
-						{title: 'Телефон', link: '#'}
-					]
+		},{
+			type: 'sidebar',
+			modkey: 'sidebar',
+			containerName: '#slSideBar',
+			content: {
+				links: [
+					{title: 'Главная', link: '#'},
+					{title: 'Продукты', link: '#'},
+					{title: 'Доставка', link: '#'},
+					{title: 'О нас', link: '#'},
+					{title: 'Телефон', link: '#'}
+				]
+			},
+			viewParams: {
+				color: {
+					text: '#000',
+					back: '#ffa',
+					border: '#000'
 				}
 			}
-		]
-	});
+		}
+	]);
 }, false);
 
 //Проверить совпадение имен модулей на странице
 //Сделать обертку
+//Разобраться с шириной модуля
